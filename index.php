030f5b804ac094846af475afa7c4e2f5a6b3214c
ktoindy <ktoindy@gmail.com>
2018-10-11 23:00:17 +0700

    release 1.0

--- index.php
+++ index.php
@@ -9,7 +9,18 @@
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.2/css/bootstrap.min.css" integrity="sha384-y3tfxAZXuh4HwSYylfB+J125MxIs6mR5FOHamPBG064zB+AFeWH94NdvaCBm8qnd" crossorigin="anonymous">
   </head>
   <body>
-    <h1>Hello, world!  5555555555+</h1>
+    <div class="container">
+      <h1 class="text-center">Example Resize Image - PHP</h1>
+      <form class="form-control" action="process.php" method="post" enctype="multipart/form-data">
+        <div class="form-group">
+          <label for="">File Photo</label>
+          <input type="file" name="image" class="form-control" value="">
+        </div>
+        <div class="form-group">
+          <input type="submit" class="btn btn-success btn-block" name="submit" value="submit">
+        </div>
+      </form>
+    </div>
 
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.2/js/bootstrap.min.js" integrity="sha384-vZ2WRJMwsjRMW/8U7i6PWi6AlO1L79snBrmgiDpgIWJ82z8eA5lenwvxbMV1PAh7" crossorigin="anonymous"></script>
